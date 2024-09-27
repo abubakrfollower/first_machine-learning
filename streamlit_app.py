c8204572e3ace8ccca9e54c0c05eb33ac4d6f491
abubakrfollower <124687760+abubakrfollower@users.noreply.github.com>
2024-09-27 19:56:24 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ with st.sidebar:
        'bill_length_mm': bill_length_mm,
        'flipper_length_mm': flipper_length_mm,
        'body_mass_g': body_mass_g,
-       'gender': gender}
+       'sex': gender}
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X], axis=0)
 
