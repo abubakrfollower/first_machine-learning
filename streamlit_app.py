038bb22a248904fd1964ac4b2c326b5c98275725
abubakrfollower <124687760+abubakrfollower@users.noreply.github.com>
2024-09-27 21:08:42 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,61 +1,64 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-st.title('Machine Learning Appp')
-st.info("This is my first app")
 
+st.title('🤖 Machine Learning App')
 
-with st.expander("data"):
-  st.write("**Raw data**")
-  df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
-  df 
+st.info('This is app builds a machine learning model!')
+
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
+
   st.write('**X**')
   X_raw = df.drop('species', axis=1)
   X_raw
+
   st.write('**y**')
   y_raw = df.species
   y_raw
-  y = df.species
-  
 
-with st.expander("Data Visualization"):
+with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+
 # Input features
 with st.sidebar:
   st.header('Input features')
-  island = st.selectbox("Island", ('Biscoe', 'Dream', 'Torgersen'))
-  
-  bill_length_mm = st.slider("Bill length (mm)", 32.1, 59.6, 43.9)
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
   bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
   flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
   gender = st.selectbox('Gender', ('male', 'female'))
+  
   # Create a DataFrame for the input features
   data = {'island': island,
-       'bill_length_mm': bill_length_mm,
-       'bill_depth_mm': bill_depth_mm,
-       'flipper_length_mm': flipper_length_mm,
-       'body_mass_g': body_mass_g,
-       'sex': gender}
+          'bill_length_mm': bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g': body_mass_g,
+          'sex': gender}
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X_raw], axis=0)
 
 with st.expander('Input features'):
-  st.write('**Input penguins**')
+  st.write('**Input penguin**')
   input_df
   st.write('**Combined penguins data**')
   input_penguins
-# Data prep
+
+
+# Data preparation
 # Encode X
 encode = ['island', 'sex']
 df_penguins = pd.get_dummies(input_penguins, prefix=encode)
 
-
 X = df_penguins[1:]
 input_row = df_penguins[:1]
 
-# Encode Y
+# Encode y
 target_mapper = {'Adelie': 0,
                  'Chinstrap': 1,
                  'Gentoo': 2}
@@ -64,13 +67,12 @@ def target_encode(val):
 
 y = y_raw.apply(target_encode)
 
-
 with st.expander('Data preparation'):
-  
-  st.write('**Encoded input penguin {x} input penguins**')
+  st.write('**Encoded X (input penguin)**')
   input_row
   st.write('**Encoded y**')
   y
+
 
 # Model training and inference
 ## Train the ML model
@@ -79,15 +81,15 @@ clf.fit(X, y)
 
 ## Apply model to make predictions
 prediction = clf.predict(input_row)
-prediction_proba = clf.predict_proba(input_row)  # Fixed this line
+prediction_proba = clf.predict_proba(input_row)
 
 df_prediction_proba = pd.DataFrame(prediction_proba)
 df_prediction_proba.columns = ['Adelie', 'Chinstrap', 'Gentoo']
 df_prediction_proba.rename(columns={0: 'Adelie',
-                                    1: 'Chinstrap',
-                                    2: 'Gentoo'})
+                                 1: 'Chinstrap',
+                                 2: 'Gentoo'})
 
-#Display predicted species
+# Display predicted species
 st.subheader('Predicted Species')
 st.dataframe(df_prediction_proba,
              column_config={
@@ -113,6 +115,7 @@ st.dataframe(df_prediction_proba,
                  max_value=1
                ),
              }, hide_index=True)
+
+
 penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
 st.success(str(penguins_species[prediction][0]))
-
