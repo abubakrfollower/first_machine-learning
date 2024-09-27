65ebd409039487bb94d08c5b86d13ea76ef40691
abubakrfollower <124687760+abubakrfollower@users.noreply.github.com>
2024-09-27 20:12:47 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,13 +46,13 @@ input_row = df_penguins[:1]
 # Encode Y
 target_mapper = {'Adelie': 0,
                  'Chinstrap': 1,
-                 'Gentra': 2}
+                 'Gentoo': 2}
 def target_encode(val):
   return target_mapper[val]
 
 y = y_raw.apply(target_encode)
 y 
-
+y_raw
 with st.expander('Input features'):
   st.write('**Input penguins**')
   input_df
