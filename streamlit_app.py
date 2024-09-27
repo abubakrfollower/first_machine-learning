ff9ae240dda0d81e15c4a09ac3da87b5e642d71b
abubakrfollower <124687760+abubakrfollower@users.noreply.github.com>
2024-09-27 20:49:42 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,5 +86,6 @@ df_prediction_proba.column = ['Adelie', 'Chinstrap', 'Gentoo']
 df_prediction_proba.rename(columns={0: 'Adelie',
                                  1: 'Chinstrap',
                                  2: 'Gentoo'})
+df_prediction_proba
 
 
